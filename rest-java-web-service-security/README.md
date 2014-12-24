371b6f832b46ed61cb9d213e96f38132cff30310
ivonet <ivonet@gmail.com>
2014-12-24 18:13:59 +0100

    Adding digest-authentication

--- rest-java-web-service-security/README.md
+++ rest-java-web-service-security/README.md
@@ -15,4 +15,13 @@ In this section you will find a few examples on how to secure your RESTFull appl
 ## Digest Authentication
 
 
+    This authentication method makes use of a hash function to encrypt the password
+    entered by the user before sending it to the server. This, obviously, makes it
+    much safer than the basic authentication method, in which the user's password
+    travels in plain text that can be easily read by whoever intercepts it. To
+    overcome such drawbacks, digest md5 authentication applies a function on the
+    combination of the values of the username, realm of application security,
+    and password. As a result, we obtain an encrypted string that can hardly
+    be interpreted by an intruder.
 
+* Read the instructions in the index.html file of the project to get it to work.
---
+++ rest-java-web-service-security/digest-authentication/src/main/java/ivonet/config/RestApplicationConfig.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2014 ivonet
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ivonet.config;
+
+
+import ivonet.services.PersonService;
+
+import javax.ws.rs.ApplicationPath;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/services")
+public class RestApplicationConfig extends javax.ws.rs.core.Application {
+
+    private final Set<Object> services;
+
+    public RestApplicationConfig() {
+        this.services = new HashSet<>();
+        this.services.add(new PersonService());
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return this.services;
+    }
+
+}
